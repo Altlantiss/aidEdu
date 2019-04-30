774e065a464cd678efccf9ffc857e67a699754db
sugejie <861031836@qq.com>
2019-04-30 16:58:42 +0800

    register

---
+++ static/js/register.js
@@ -0,0 +1,8 @@
+$(function() {
+	$("#city-picker").cityPicker({
+		toolbarTemplate: '<header class="bar bar-nav">\
+			<button class="button button-link pull-right close-picker">确定</button>\
+			<h1 class="title">选择省市</h1>\
+			</header>'
+	});
+});
